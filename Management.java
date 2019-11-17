ef4506dc325f10de9f1cce1dbfd625b0998d0196
fire1706 <46523392+fire1706@users.noreply.github.com>
2019-11-17 13:39:47 +0100

    Update Management.java

--- Management.java
+++ Management.java
@@ -1,1 +1,14 @@
-// c'est Le run ou chque fonction est appelé pour faire ce qu'elle a a faire 
+// c'est Le run ou chque fonction est appelé pour faire ce qu'elle a a faire
+
+import java.io.*;
+import java.net.*;
+import java.lang.*;
+import java.util.*;
+
+public class Management extends Thread {
+
+    Socket socketManagement;
+    Game ( Socket _s){socketManagement=_s;}
+
+    @Override
+    public void run(){
