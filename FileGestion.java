6dd92c1a6f032543ab5a6610c30a5ba77de51b03
victordachet <victor.dachet@hotmail.com>
2019-11-25 20:39:42 +0100

    Update FileGestion.java
    Pas encore fini loin de là mais besoin de réflexion pour continuer. Je dois réfléchir à comment gérer les différentes commandes

--- FileGestion.java
+++ FileGestion.java
@@ -15,3 +15,35 @@
 // LIST
 // PWD
 //
+
+import java.net.*;
+import java.io.*;
+
+public class FileGestion{
+	int authorized = -1;
+	Socket socketClient = null;
+	FileGestion(Socket socketClient, int authorized){
+		this.socketClient = socketClient;
+		this.authorized = authorized;
+	}
+
+	public void menu(){
+		try{
+			System.out.println("Welcome in the menu:");
+			System.out.println("Send HELP if you need some help.");
+
+			InputStream inStream = socketClient.getInputStream();
+          	OutputStream outStream = socketClient.getOutputStream();
+
+          	BufferedReader input = new BufferedReader(new InputStreamReader(inStream));
+          	String inString = input.readLine();
+		}catch(UnknownHostException e){
+			e.printStackTrace();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
+		
+
+
+	}
+}
