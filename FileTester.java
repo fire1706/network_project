97a415510a9c50a0438ca2f877a8be8da87989ce
victordachet <victor.dachet@hotmail.com>
2019-12-10 19:39:14 +0100

    Create FileTester.java
    Permet de tester FileTest

---
+++ FileTester.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import javax.imageio.ImageIO;
+
+public class FileTester{
+	public static void main(String[] args){
+		FileTest ft = new FileTest();
+		ft.getPathname();
+	}
+}
