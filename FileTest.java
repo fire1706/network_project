4d0adcbf034dc61aa9746234c084ec1944286391
victordachet <victor.dachet@hotmail.com>
2019-12-10 19:40:03 +0100

    Create FileTest.java
    Permet de créer des fichiers virtuels et de comprendre le fonctionnement des méthodes. Servira plus tard à créer nos fichiers virtuels depuis le server

---
+++ FileTest.java
@@ -0,0 +1,65 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import javax.imageio.ImageIO;
+
+public class FileTest{
+	File file = new File("/");
+	File privateFile = new File("/","private");
+	File mytext = new File("/", "mytext.txt");
+	File myImage = new File("/", "myimage.bmp");
+	File secret = new File("/private", "secret.txt");
+	
+
+	byte[] myImg = {66,  77,  70,  1,  0,  0,    0,   0,   0,   0,  62,   0,   0,  0,   40,   0,
+                         0,   0,  34,  0,  0,  0,   33,   0,   0,   0,   1,   0,   1,  0,    0,   0,
+                         0,   0,   8,  1,  0,  0,    0,   0,   0,   0,   0,   0,   0,  0,    0,   0,
+                         0,   0,   0,  0,  0,  0,    0,   0,   0,   0,  -1,  -1,  -1,  0,   -1,  -1,
+                        -1,  -1, -64,  0,  0,  0,   -1, -32,   0,   1, -64,   0,   0,  0, -128,  31,
+                        -1,  -2, -64,  0,  0,  0,  -65, -33,  -1,  -2, -64,   0,   0,  0,  -65, -33,
+                        -1,  -2, -64,  0,  0,  0,  -65, -33,  -1,  -8, -64,   0,   0,  0,  -65, -33,
+                        -1,  -2, -64,  0,  0,  0,  -65, -33,  -1,  -2, -64,   0,   0,  0,  -65, -33,
+                        -1,  -2, -64,  0,  0,  0,  -65, -33,  -1,  -2, -64,   0,   0,  0,  -65, -33,
+                        -1,  -2, -64,  0,  0,  0,  -65, -33,  -1,  -8, -64,   0,   0,  0,  -65, -33,
+                        -1,  -2, -64,  0,  0,  0,  -65, -33,  -1,  -2, -64,   0,   0,  0,  -65, -33,
+                        -1,  -2, -64,  0,  0,  0,  -65, -33,  -1,  -2, -64,   0,   0,  0,  -65, -33,
+                        -1,  -2, -64,  0,  0,  0, -128,  31,  -1,  -8, -64,   0,   0,  0,   -1, -17,
+                        -1,  -2, -64,  0,  0,  0,   -1,  -9,  -1,  -2, -64,   0,   0,  0,   -1,  -5,
+                        -1,  -2, -64,  0,  0,  0,   -1,  -3,  -2,   0, -64,   0,   0,  0,   -1,  -2,
+                        -3,  -1, -64,  0,  0,  0,   -1,  -1, 126,  -1, -64,   0,   0,  0,   -1,  -1,
+                       127, 127, -64,  0,  0,  0,   -1,  -1, -65, 127, -64,   0,   0,  0,   -1,  -1,
+                       -33, -65, -64,  0,  0,  0,   -1,  -1, -17, -65, -64,   0,   0,  0,   -1,  -1,
+                       -17, -33, -64,  0,  0,  0,   -1,  -1,  -9, -33, -64,   0,   0,  0,   -1,  -1,
+                        -9, -33, -64,  0,  0,  0,   -1,  -1,  -8,  31, -64,   0,   0,  0,   -1,  -1,
+                        -1,  -1, -64,  0,  0,  0
+                        
+        };
+
+        FileTest(){
+
+
+        }
+
+        public void getPathname(){
+        	//secret.createNewFile();
+        	try{
+        		
+        		secret.createNewFile();
+        	}
+        	catch(IOException e){
+        		e.printStackTrace();
+        	}catch(SecurityException e){
+        		e.printStackTrace();
+        	}
+        	
+        	System.out.println(file.toString());
+
+        	System.out.println(myImage.toString());
+
+        	System.out.println(secret.isDirectory());
+        	System.out.println(secret.isFile());
+        }
+
+
+
+}
