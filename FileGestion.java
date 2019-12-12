46e6edea0871c6dfa2542b85ebc85ede9d1ad7ba
victordachet <victor.dachet@hotmail.com>
2019-12-12 12:56:14 +0100

    Update FileGestion.java

--- FileGestion.java
+++ FileGestion.java
@@ -23,10 +23,15 @@ import java.util.*;
 public class FileGestion{
 	int authorized = -1;
 	Socket socketClient = null;
+	Node currentNode = null;
+
+	//Constructor
 	FileGestion(Socket socketClient, int authorized, FileVirtuel file_virtuel){
 		this.socketClient = socketClient;
 		this.authorized = authorized;
+		this.currentNode = file_virtuel.root;
 	}
+
 
 	public void menu(){
 		String str = new String();
@@ -41,21 +46,49 @@ public class FileGestion{
           	String inString = new String();
           	while(true){
 				inString = input.readLine();
+				System.out.println("boucle du menu FileGestion");
+				System.out.println("Commande recue" + inString);
 
 				if(inString.contains("PWD")){
 					outStream.write("257 /\r\n".getBytes());
 				}
 				else if(inString.contains("LIST")){
 					System.out.println("ici");
-					outStream.write("110 private mytext.txt myimage.bmo\r\n".getBytes());// ici je pense pas que se sois la commande 110 vic
+					outStream.write("212 list comes:\r\n".getBytes());
+					int sizeOfCurrentNode = currentNode.getSizeContent();
+					String[] contentOfCurrentNode = new String[sizeOfCurrentNode];
+					String toSend = null;
+					try{
+						contentOfCurrentNode = currentNode.getDirectoryContent();
+					}catch(NodeException e){
+						contentOfCurrentNode = null;
+					}
+					
+
+					for(int i = 0; i<sizeOfCurrentNode; i++){
+						toSend = "/" + contentOfCurrentNode[i] + "\r\n";
+						outStream.write(toSend.getBytes());
+					}
+					//outStream.write("212 End of list\r\n".getBytes());
+					System.out.println("Content must be send to Client");
+
+
+
+					// ici je pense pas que se sois la commande 110 vic Bien vu Thom MERCI!!
 				}
 				else if(inString.contains("SYST")){
 					Properties prop = new Properties();
 					prop = System.getProperties();
 					str = prop.getProperty("os.arch");
-					outStream.write("215 \r\n".getBytes());
+					outStream.write("215\r\n".getBytes());
 				}
 				else if(inString.contains("HELP")){
+
+				}else if(inString == null){
+					outStream.write("500\r\n".getBytes());
+
+				}else{
+					outStream.write("502\r\n".getBytes());
 
 				}
 
