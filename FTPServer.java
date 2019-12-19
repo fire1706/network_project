db98606d95b47d5a8c98803dce144209751b758e
victordachet <victor.dachet@hotmail.com>
2019-12-19 03:24:49 +0100

    Update FTPServer.java

--- FTPServer.java
+++ FTPServer.java
@@ -1,8 +1,8 @@
 //---------------------------------FTPServer---------------------------------------------
 //
 // This first function goal is to launch the ftp server (in his globality).
-// It will create the virtual file asked and allow us to launch a fixed
-// number of threads when program is called.
+// It creates the virtual files asked and allows us to launch a fixed
+// number of threads when the program is called.
 //
 //Copyright (c) 2019 by Thomas BASTIN & Victor Dachet. All Rights Reserved.
 //-----------------------------------------------------------------------------------------
@@ -15,8 +15,6 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 import java.net.*;
-import java.awt.*;
-import javax.imageio.ImageIO;
 
 public class FTPServer{
 
@@ -33,7 +31,7 @@ public class FTPServer{
     }catch(NumberFormatException e){
       System.out.println("We couldn't parse correctly the maxThreads number ! ");
     }// end try&catch
-    // We fixe the number thread pool
+    // We fix the number thread pool
     ExecutorService exe = Executors.newFixedThreadPool(maxThread);
 
     // Instantiation of virtual file
@@ -51,18 +49,12 @@ public class FTPServer{
         managementCommandSocket = commandSocket.accept();
         managementCommandSocket.setSoTimeout(300000); // timeout define to wait x second , if it is reach the Socket closed
 
-
-        // We say that the server is ready to get command and data
-        System.out.println("Ok let's go ");
-
         // We launch a new thread
         System.out.println("New connection incoming");
         
         exe.execute(new Management(managementCommandSocket, file_virtuel));
         
-        
       }// end while
-
 
     }catch(IOException e){
       System.out.println(e.getMessage());
@@ -70,50 +62,3 @@ public class FTPServer{
     exe.shutdown();
   }//end main function
 }// end class
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    // A  faire : on doit ouvrir le socket du server puis dans un try and catch
-    //  recevoir les donnée du client et le lancer ou sur un connection passive ou active
-    // ensuite ouvrir autre fonction avec un extends threads ou le client se connecte
-    // et fais ces truc ( en gros faire un run() ) lorsque que celui-ci se connecte
-    // on lui donne un certain argument pour voir si il peut oui ou non aller voir les fichier privé
-    // Notre Protocol devra pourvoir faire plusieurs truc on va donc implémenter
-    // ou une class contenant plusieurs function utile ou alors une class pour chaque
-    // fonction .( perso je pense une classe pour chaque remplie de petit opération définie dans une plus grand class avec des function pour chacune de ces petite opération)
-    //
-    //Attention a pas oublier les commentaire dans les codes et une intro a chque code
-    //
-    // ne pas oublier les opérations dans l'assignement page 6 (au dessus)
-    //
-    // c'est tout ce que j'ai a dire a toi Vic
-    //
-    //
