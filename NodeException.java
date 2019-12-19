876623e1436208c707c76be18d4634cb5b8f0782
victordachet <victor.dachet@hotmail.com>
2019-12-19 03:35:16 +0100

    Update NodeException.java

--- NodeException.java
+++ NodeException.java
@@ -1,3 +1,9 @@
+//---------------------------------NodeException---------------------------------------------
+//
+// This object has for purpose to lauchn exception for Node.java class
+//
+//Copyright (c) 2019 by Thomas BASTIN & Victor Dachet. All Rights Reserved.
+//-----------------------------------------------------------------------------------------
 
 @SuppressWarnings("serial")
 public class NodeException extends Exception{
